3826db0a345c4b5690dd2cf87ac41fb669ee1057
Marketa Cechova <marketa.sebestova@gmail.com>
2023-10-07 16:22:11 +0200

    styly a reset formuláře

--- src/components/Registration/index.jsx
+++ src/components/Registration/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "../../style.css";
+import "./style.css";
 
 const Registration = () => {
   const [user, setUser] = useState({
@@ -18,55 +18,63 @@ const Registration = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(user);
+    setUser({
+      username: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    }); 
   };
 
   return (
     <div className="container">
-      <h1>REGISTRATION</h1>
-      <form className="form" onSubmit={handleSubmit}>
-        <input
-          className="input"
-          type="email"
-          value={user.email}
-          name="email"
-          placeholder="Email Address"
-          onChange={(e) => setUser({ ...user, email: e.target.value })}
-          onBlur={handleBlur}
-        />
+      <h2>REGISTRATION</h2>
+      <div className="formContainer">
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            className="input"
+            type="email"
+            value={user.email}
+            name="email"
+            placeholder="Email Address"
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            onBlur={handleBlur}
+          />
 
-        <input
-          className="input"
-          type="text"
-          value={user.username}
-          name="username"
-          placeholder="User Name"
-          onChange={(e) => setUser({ ...user, username: e.target.value })}
-        />
+          <input
+            className="input"
+            type="text"
+            value={user.username}
+            name="username"
+            placeholder="User Name"
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+          />
 
-        <input
-          className="input"
-          type="password"
-          value={user.password}
-          name="password"
-          placeholder="Password"
-          onChange={(e) => setUser({ ...user, password: e.target.value })}
-        />
+          <input
+            className="input"
+            type="password"
+            value={user.password}
+            name="password"
+            placeholder="Password"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
 
-        <input
-          className="input"
-          type="password"
-          value={user.passwordConfirm}
-          name="confirmPassword"
-          placeholder="Confirm Password"
-          onChange={(e) =>
-            setUser({ ...user, passwordConfirm: e.target.value })
-          }
-        />
-
-        <button className="button" type="submit">
-          REGISTER
-        </button>
-      </form>
+          <input
+            className="input"
+            type="password"
+            value={user.passwordConfirm}
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            onChange={(e) =>
+              setUser({ ...user, passwordConfirm: e.target.value })
+            }
+          />
+      
+          <button className="button" type="submit">
+            REGISTER
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
